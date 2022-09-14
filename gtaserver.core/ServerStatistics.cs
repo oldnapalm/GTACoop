22788463a3598283504afd52b1264f2afa10007b
Indra <wuppie55@gmail.com>
2022-09-14 18:25:48 +0200

    Update server packages

--- gtaserver.core/ServerStatistics.cs
+++ gtaserver.core/ServerStatistics.cs
@@ -2,6 +2,9 @@
 {
     public class ServerStatistics
     {
+        /// <summary>
+        /// Gets the total connections made to the server excluding discovery
+        /// </summary>
         public int TotalConnections { get; internal set; }
     }
 }
